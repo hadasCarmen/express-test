96e5830b0c5f1c2ce8956fc51eb58a189a4f4208
elazar <tkgzreuckr@gmail.com>
2025-12-25 09:37:12 +0200

    first function

--- controlers.js
+++ controlers.js
@@ -1,1 +1,37 @@
 import fs from "fs/promises";
+
+async function readUsers() {
+  try {
+    const data = await fs.readFile("users.json", "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
+}
+
+async function writeUsers(users) {
+  await fs.writeFile("users.json", JSON.stringify(users, null, 2));
+}
+
+export const reginUser = async (req, res) => {
+  const { username, password } = req.body;
+  const users = await readUsers();
+
+  if (users.some((u) => u.username === username)) {
+    return res.status(400).json({ message: "Username already exists" });
+  }
+
+  const maxId = users.length > 0 ? Math.max(...users.map((u) => u.id)) : 0;
+
+  const newUser = {
+    id: maxId + 1,
+    username,
+    password,
+  };
+
+  users.push(newUser);
+  await writeUsers(users);
+
+  const { password: _, ...userResponse } = newUser;
+  res.status(201).json(userResponse);
+};
--- routers.js
+++ routers.js
@@ -1,5 +1,13 @@
 import express from "express";
-
+import{
+    reginUser
+} from "./controlers.js"
 const router = express.Router();
+
+router.route("/user/register").post(reginUser)
+
+
+
+
 
 export default router;
