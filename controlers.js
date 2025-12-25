f619104169348215e329b61b5ebb9c6749bc0870
elazar <tkgzreuckr@gmail.com>
2025-12-25 11:04:52 +0200

    function buy ticket

--- controlers.js
+++ controlers.js
@@ -29,10 +29,7 @@ export const reginUser = async (req, res) => {
     return res.status(400).json({ message: "Username already exists" });
   }
 
-  const maxId = users.length > 0 ? Math.max(...users.map((u) => u.id)) : 0;
-
   const newUser = {
-    id: maxId + 1,
     username,
     password,
   };
@@ -52,9 +49,20 @@ async function readEvent() {
     return [];
   }
 }
-
 async function writeEvents(events) {
   await fs.writeFile("events.json", JSON.stringify(events, null, 2));
+}
+async function readReceipts() {
+  try {
+    const data = await fs.readFile("receipts.json", "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    return [];
+  }
+}
+
+async function writeReceipts(receipts) {
+  await fs.writeFile("receipts.json", JSON.stringify(receipts, null, 2));
 }
 
 // app.post("/login", async (req, res) => {
@@ -77,8 +85,8 @@ async function writeEvents(events) {
 //   res.json(events);
 // });
 
-export const createEvent= async (req, res) => {
-  const { username, password, eventName, ticketsForSale } = req.body;
+export const createEvent = async (req, res) => {
+  const { username, password, eventName, ticketsAvailable } = req.body;
 
   const user = await validateUser(username, password);
   if (!user) {
@@ -91,14 +99,48 @@ export const createEvent= async (req, res) => {
   const maxId = events.length > 0 ? Math.max(...events.map((p) => p.id)) : 0;
 
   const newEvent = {
-    id: maxId + 1,
     eventName,
-    ticketsForSale,
-    authorId: user.id,
-    authorUsername: user.username,
+    ticketsAvailable,
+    createdBy: user.username,
   };
 
   events.push(newEvent);
   await writeEvents(events);
   res.status(201).json({ message: "Event created successfully" });
+};
+
+export const buyTickets = async (req, res) => {
+  const { username, password, eventName, quantity } = req.body;
+  const user = await validateUser(username, password);
+  if (!user) {
+    return res
+      .status(401)
+      .json({ message: "Unauthorized: Invalid username or password" });
+  }
+  const events = await readEvent();
+  const event = events.find(
+    (e) => e.eventName.toUpperCase() === eventName.toUpperCase()
+  );
+
+  if (event.ticketsAvailable >= quantity) {
+    event.ticketsAvailable -= quantity;
+
+    const newReceipt = {
+      username: username,
+      eventName: eventName,
+      ticketsBought: quantity,
+    };
+    const eventToUpdute = events.findIndex(
+      (even) => even.eventName === event.eventName
+    );
+    events[eventToUpdute] = event;
+    writeEvents(events);
+    const receipts = await readReceipts();
+    receipts.push(newReceipt);
+    writeReceipts(receipts);
+
+    res.status(201).json({ message: "Tickets purchased successfully" });
+  } else {
+    res.status(404).send({ message: "not enouf tickets" });
+  }
 };
--- routers.js
+++ routers.js
@@ -1,15 +1,9 @@
 import express from "express";
-import{
-    createEvent,
-    reginUser
-} from "./controlers.js"
+import { buyTickets, createEvent, reginUser } from "./controlers.js";
 const router = express.Router();
 
-router.route("/user/register").post(reginUser)
-router.route("/creator/events").post(createEvent)
-
-
-
-
+router.route("/user/register").post(reginUser);
+router.route("/creator/events").post(createEvent);
+router.route("/users/tickets/buy").post(buyTickets);
 
 export default router;
